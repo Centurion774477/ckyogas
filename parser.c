14c50bd5fe8faf99e8bb3b4ec68e44f65334cc9c
Reece Turner <reeceturner1358@gmail.com>
2026-09-07 17:39:01 -0400

    fixed guard clause

--- parser.c
+++ parser.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 void parse(char* file) {
-    FILE * file = fopen(file, "r");
-    if (fp == NULL) {   
-        printf("external.fileSys: The file you tried to parse does not exist in this context.");
+    FILE *file = fopen(file, "r");
+    if (file == NULL) {   
+        printf("external.fileSys: something went wrong with trying to open %s.", file);
+        printf("This could be triggered by two things: the file either doesn't exist");
+        printf("--or the file's permissions blocked Kyogas.");
         return;
     }
     else {
